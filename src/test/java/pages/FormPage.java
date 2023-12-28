fe5ec5f812c3efb25f0d5954a661ca6bdbdc796f
Prabhdeep Singh <prabhdeepsingh329@gmail.com>
2023-12-28 15:49:49 +0530

    Parametrized Experience in Form
    Parametrized Experience in Form

--- src/test/java/pages/FormPage.java
+++ src/test/java/pages/FormPage.java
@@ -1,10 +1,11 @@
 package pages;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import utility.RandomData;
 public class FormPage {
-	WebDriver driver;
+	private WebDriver driver;
 	public FormPage(WebDriver driver) {
 		this.driver = driver;
 	}
@@ -14,6 +15,7 @@ public class FormPage {
 	private By jobTitle = By.id("job-title");
 	private By radios = By.xpath("//input[@type='radio']");
 	private By checkBox = By.xpath("//input[@type='checkbox']");
+	private By selectOptions = By.xpath("//option");
 	private By selectJob(int i) {
 		return By.xpath("(//input[@type='radio'])["+i+"]");
 	}
@@ -31,7 +33,8 @@ public class FormPage {
 		driver.findElement(selectJob(randomJob)).click();
 		driver.findElement(selectGender(randomGender)).click();
 		Select selectMenu = new Select(driver.findElement(By.id("select-menu")));
-		selectMenu.selectByIndex(2);
+		int randomExperience= random.getRandomNumber(driver.findElements(selectOptions).size());
+		selectMenu.selectByIndex(randomExperience);
 		driver.findElement(enterDate).sendKeys(random.createRandomDate());
 		driver.findElement(submit).click();
 	}
